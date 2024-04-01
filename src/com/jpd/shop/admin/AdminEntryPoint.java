783e3c0ae8deb616cdce989390ef30d9835c85bb
IohannesPaullusMD <141614774+IohannesPaullusMD@users.noreply.github.com>
2024-04-01 17:05:35 +0800

    initial commit

---
+++ src/com/jpd/shop/admin/AdminEntryPoint.java
@@ -0,0 +1,12 @@
+package com.jpd.shop.admin;
+
+/**
+ *
+ * @author jpd
+ */
+public class AdminEntryPoint {
+    public static void main(String[] args) {
+        new MainFrame().setVisible(true);
+
+    }
+}
---
+++ src/com/jpd/shop/admin/Colors.java
@@ -0,0 +1,28 @@
+package com.jpd.shop.admin;
+
+import java.awt.Color;
+
+public class Colors {
+
+    /** pinaka-white ha ngatanan nga white nga akon gagamiton */
+    public static final Color WHITE = new Color(0xFFFFFF);
+
+    /** an white nga gagamiton kun ma hover ha mga buttons */
+    public static final Color WHITE_HOVER = new Color(0xF2F2F2);
+
+    /** pan background la nga white */
+    public static final Color WHITE_BACKGROUND = new Color(0xF0F2F5);
+
+    /** gagamiton ini nga color para ha mga dividers */
+    public static final Color GRAY = new Color(0xCED0D4);
+
+    /** ano man ini nga color? */
+    public static final Color BLUE = new Color(0x0866FF);
+
+    /** gamiton ini para ha mga texts */
+    public static final Color BLACK = new Color(0x050505);
+
+    // 65676B para color hit icons
+    // F02849 red
+    // 45BD62 green
+}
