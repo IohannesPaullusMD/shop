77bc0b0354db4713d2c3321f3e3b9300d8ae5dae
IohannesPaullusMD <141614774+IohannesPaullusMD@users.noreply.github.com>
2024-04-15 21:00:16 +0800

    fixed server executable jar file where server socket  is always closed

--- src/com/jpd/shop/server/source/Server.java
+++ src/com/jpd/shop/server/source/Server.java
@@ -7,23 +7,28 @@ import java.net.Socket;
 public class Server implements Runnable {
     private ServerSocket serverSocket;
 
-    public Server(ServerSocket serverSocket) {
-        this.serverSocket = serverSocket;
+    public Server(/* ServerSocket serverSocket */) throws IOException {
+        this.serverSocket = new ServerSocket(55555);
+        MainFrame.jLabel1.setText(Boolean.toString(this.serverSocket.isClosed()));
     }
 
     @Override
     public void run() {
+        // MainFrame.jLabel1.setText(Boolean.toString(this.serverSocket.isClosed()));
         while (!serverSocket.isClosed()) {
             try {
+                MainFrame.jLabel1.setText("running");
                 Socket socket = serverSocket.accept();
                 ClientHandler clientHandler = new ClientHandler(socket);
                 Thread thread = new Thread(clientHandler);
                 thread.start();
             } catch (IOException e) {
                 e.printStackTrace();
-                closeServerSocket();
+                MainFrame.jLabel1.setText(e.getMessage());
+                // closeServerSocket();
             }
         }
+        // MainFrame.jLabel1.setText("End");
     }
 
     void closeServerSocket() {
@@ -34,6 +39,8 @@ public class Server implements Runnable {
             }
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            MainFrame.jLabel1.setText("error");
         }
 
     }
